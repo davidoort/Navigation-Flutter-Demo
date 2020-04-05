8937b13065f0febed0c9a119cbcddc1ad3f55540
David Oort Alonso <davidoort@hotmail.com>
2020-04-05 08:18:36 +0200

    basic implementation

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -12,7 +12,10 @@ class Screen2 extends StatelessWidget {
         child: RaisedButton(
           color: Colors.blue,
           child: Text('Go Back To Screen 1'),
-          onPressed: () {},
+          onPressed: () {
+            // TODO: check the navigator route stack and push or pop depending on that (I want to avoid a very long history when going to screen 2 then 1 then 2 then 1, etc)
+            Navigator.pushNamed(context, '/first');
+          },
         ),
       ),
     );
